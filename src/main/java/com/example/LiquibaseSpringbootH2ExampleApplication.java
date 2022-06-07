3b85a067b369ac08401cb76ec34f9772aff5d48c
Deepak Bhalla <deepak.dbhalla@gmail.com>
2022-06-07 14:30:54 +0530

    Add files via upload

---
+++ src/main/java/com/example/LiquibaseSpringbootH2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSpringbootH2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseSpringbootH2ExampleApplication.class, args);
+	}
+
+}
